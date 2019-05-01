4cdfd6b7d20ec421e35227792bdcdd7f85ffa630
Suzumiya Haruhi <mswdwhu@outlook.com>
2019-05-01 20:26:23 +0800

    update

---
+++ src/com/company/Solution046.java
@@ -0,0 +1,15 @@
+/**
+ * @author suzumiya
+ * @data 5/1/19 8:20 PM
+ */
+
+package com.company;
+
+import java.util.List;
+
+public class Solution046 {
+    public List<List<Integer>> permute(int[] nums) {
+
+        return null;
+    }
+}
