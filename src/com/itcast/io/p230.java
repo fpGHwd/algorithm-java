60c36ab800bec2ef1b2ffacbaf7bf2694681f048
Suzumiya Haruhi <mswdwhu@outlook.com>
2019-05-14 22:26:17 +0800

    :trollface: update

---
+++ src/com/itcast/io/p230.java
@@ -0,0 +1,35 @@
+/*
+ * Created by Intellij IDEA.
+ * @Author:     suzumiya
+ * @Date:       5/14/19
+ * @Time:       8:01 PM
+ * @Project:    algorithm
+ */
+
+package com.itcast.io;
+
+public class p230 {
+
+    /**
+     * ## string buffer
+     *
+     * - string buffer是一个容器
+     * - 创建缓冲区
+     * - 清空缓冲区
+     *
+     * ### IO stream
+     *
+     * - 文件，网络
+     * - 字节流（ASCII），字符流（融入编码表，编码由流指定）
+     * - 字节流基类（InputStream，OutputStream），字符流基类（Reader，Writer）
+     * - 操作文件
+     * - io异常是普遍的异常：与设备文件发生操作的都会带有IO异常
+     * - 对io异常进行处理
+     * - 水滴-字符读。太慢。杯子对水的缓冲作用。缓冲BufferReader，BufferWriter。缓冲区结合流来使用。
+     * - p230缓冲区的出现是为了提升流的操作效率，所以先要有stream对象
+     * - fliter的实现本质也是流的buffer链。
+     * - 换行符号不一样，newLine();
+     * - p231 文 本yi行的形式存在
+     */
+
+}
--- src/com/leetcode/P043.java
+++ src/com/leetcode/P043.java
@@ -8,14 +8,42 @@
 
 package com.leetcode;
 
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class P043 {
 
-    public String multiply(String num1, String num2) {
-
-        return "";
+    public static String multiply(String num1, String num2) {
+        if("0".equals(num1) || "0".equals(num2))return "0";
+        int len1 = num1.length(), len2 = num2.length();
+        char[] ca = new char[len1+len2];
+        int carry = 0;
+        for(int i = 1; i<= len1+len2-1; i++){
+            int n = carry;
+            for(int j = 1;j<=len1;j++){
+                if(i+1-j < 1 || i+1-j > len2)continue;
+                int n1 = len1-j, n2 = (len2-1) - (i-j);
+                n += (num1.charAt(n1) - '0')*(num2.charAt(n2) -'0');
+            }
+            carry = n/10;
+            ca[i-1] = (char)(n%10 + '0');
+        }
+        int len = len1+len2-1;
+        if(carry!=0){
+            ca[len1+len2-1] = (char)(carry+'0');
+            len++;
+        }
+        StringBuilder sb = new StringBuilder();
+        while(len>0){
+            sb.append(ca[len-1]);
+            len--;
+        }
+        return sb.toString();
     }
 
     public static void main(String[] args){
-
+        String s1 = "25", s2 = "30";
+        System.out.println(multiply(s1,s2));
     }
 }
