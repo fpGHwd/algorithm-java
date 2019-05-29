449ebfa2007decd7aaf81393a0591b00e68f1da0
suzumiya <ggwdwhu@gmail.com>
2019-05-29 19:17:32 +0800

    quick sort

---
+++ src/com/jzoffer/P8.java
@@ -0,0 +1,38 @@
+/**
+ * Created by Intellij IDEA.
+ *
+ * @Author: suzumiya
+ * @Project: HydroCloud
+ * @Date: 5/29/19
+ * @Time: 5:54 PM
+ */
+
+package com.jzoffer;
+
+public class P8 {
+    public static void main(String[] args) {
+        int[] array = {1,4,2,5,2,8,4,2,7,1,3};
+        quickSort(array, 0, array.length-1);
+    }
+
+    static int partion(int[] arr, int start, int end){
+        if(start >= end)return start;
+        int t = arr[start];
+        while(start < end){
+            while(start < end && arr[end] >= t)end--;
+            arr[start] = arr[end];
+            while(start < end && arr[start]<= t)start++;
+            arr[end] = arr[start];
+        }
+        arr[start]= t;
+        return start;
+    }
+
+    static void quickSort(int[] arr, int start, int end){
+        int k = partion(arr , start,end);
+        if(k > start)
+            quickSort(arr,start,k-1);
+        if(k < end)
+        quickSort(arr, k+1, end);
+    }
+}
